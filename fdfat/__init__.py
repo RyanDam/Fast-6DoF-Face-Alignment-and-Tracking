521edbbeb68fc160e07b38fa5b6f690c805fd6b5
ryandam <py.ryan.dam@gmail.com>
2023-07-16 12:27:14 +0700

    fix wheel package not include default config

--- fdfat/__init__.py
+++ fdfat/__init__.py
@@ -1,9 +1,8 @@
 import platform
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 LOGGING_NAME = 'landmark'
 VERBOSE = 'true'
 MACOS, LINUX, WINDOWS = (platform.system() == x for x in ['Darwin', 'Linux', 'Windows'])  # environment booleans
 TQDM_BAR_FORMAT = '{l_bar}{bar:10}{r_bar}'
-
