5f4a93a857f3f4552887155b57a8501723e89ba3
ryandam <py.ryan.dam@gmail.com>
2023-07-31 16:06:28 +0700

    experiments

--- fdfat/nn/model.py
+++ fdfat/nn/model.py
@@ -10,6 +10,7 @@ from . import module2
 from . import module3
 from . import module4
 from . import module5
+from . import module6
 
 class BaseModel(nn.Module):
 
@@ -271,6 +272,53 @@ class LightWeightDirectModel(BaseModel):
         return x
 
 class LightWeightDirectModelSiLU(LightWeightDirectModel):
+
+    def __init__(self, imgz=128, muliplier=1, pose_rotation=False, act=True):
+        super().__init__(imgz=imgz, muliplier=muliplier, pose_rotation=pose_rotation, act=nn.SiLU())
+
+class LightWeightCSPModel3(BaseModel):
+
+    default_act = nn.ReLU6()  # default activation
+
+    def __init__(self, imgz=128, muliplier=1, pose_rotation=False, act=True):
+        super().__init__()
+        self.pose_rotation = pose_rotation
+
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+        # backbone, output size = size/4
+        self.backbone = module6.LightWeightBackbone(muliplier=muliplier, act=self.act)
+
+        self.mainstream = module6.MainStreamModule(muliplier=muliplier, act=self.act)
+
+        if pose_rotation:
+            self.aux = module6.AuxiliaryBackbone(int(32*muliplier), 3, muliplier=muliplier, act=self.act)
+            
+        output_ch = int((96 + 96 + 96)*muliplier)
+        self.logit = module6.FERegress(output_ch, 70*2)
+
+        if self.pose_rotation:
+            self.concat = Concat()
+
+        initialize_weights(self)
+
+    def forward(self, x):
+
+        x = self.backbone(x)
+        
+        if self.pose_rotation:
+            aux = self.aux(x)
+
+        x = self.mainstream(x)
+        
+        x = self.logit(x)
+
+        if self.pose_rotation:
+            x = self.concat([x, aux])
+
+        return x
+
+class LightWeightCSPModel3SiLU(LightWeightCSPModel3):
 
     def __init__(self, imgz=128, muliplier=1, pose_rotation=False, act=True):
         super().__init__(imgz=imgz, muliplier=muliplier, pose_rotation=pose_rotation, act=nn.SiLU())
